9577e4d5587a4d8e94c1ec8b15a0ae7c01c4975d
EdemBatalow <batalow.e.i23@gmail.com>
2024-11-12 19:21:45 +0300

    пункт 4_2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Жанр рок*/
+router.get('/rok', function(req, res, next) {
+  res.send("<h1>Жанр рок</h1>")
+  });
+ 
+  /* Жанр фонк */
+router.get('/phonk', function(req, res, next) {
+  res.send("<h1>Жанр фонк</h1>")
+  });
+
+  /* Жанр поп */
+  router.get('/pop', function(req, res, next) {
+  res.send("<h1>Жанр поп</h1>")
+  });
+
 module.exports = router;
