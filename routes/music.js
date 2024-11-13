ca20ff69cbda25767300f4dc92515a22cc9e9725
EdemBatalow <batalow.e.i23@gmail.com>
2024-11-13 21:20:26 +0300

    пункт 8_3

--- routes/music.js
+++ routes/music.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var music = require('../models/music').music;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,6 +10,17 @@ router.get('/', function(req, res, next) {
 router.get("/:nick", function(req, res, next) {
     res.send(req.params.nick);
     });
-    
+
+    router.get("/:nick", async function(req, res, next) {
+        var musics = await music.find({nick: req.params.nick});
+        console.log(musics)
+        if(!musics.length) return next(new Error("Нет такого жанра"))
+        var music = musics[0];
+        res.render('music', {
+        title: music.title,
+        picture: music.avatar,
+        desc: music.desc
+        })
+        });    
 
 module.exports = router;
