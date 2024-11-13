ab4ea131ceeac09de7f0e7e556a9305026f4c9d8
EdemBatalow <batalow.e.i23@gmail.com>
2024-11-13 23:42:05 +0300

    пункт 8_4

--- routes/music.js
+++ routes/music.js
@@ -1,26 +1,24 @@
 var express = require('express');
 var router = express.Router();
-var music = require('../models/music').music;
+var Music = require('../models/music').Music;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маршрутов, начинающихся с musics');
-});
-/* Страница музыки */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
+    res.send('Новый маршрутизатор, для маршрутов, начинающихся с musics');
     });
 
-    router.get("/:nick", async function(req, res, next) {
-        var musics = await music.find({nick: req.params.nick});
-        console.log(musics)
-        if(!musics.length) return next(new Error("Нет такого жанра"))
-        var music = musics[0];
-        res.render('music', {
-        title: music.title,
-        picture: music.avatar,
-        desc: music.desc
-        })
-        });    
+router.get("/:nick", async function(req, res, next) {
+    var musics = await Music.find({nick: req.params.nick});
+    console.log(musics)
+    if(!musics.length) return next(new Error("Нет такого жанра"))
+    var music = musics[0];
+    res.render('music', {
+    title: music.title,
+    picture: music.avatar,
+    desc: music.desc
+    })
+    });
+       
 
 module.exports = router;
+
