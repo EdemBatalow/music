502fe5cbb8e854c421817020c7e8abf71d3ba2bc
EdemBatalow <batalow.e.i23@gmail.com>
2024-11-14 00:18:21 +0300

    пункт 9_2

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
+  res.cookie('greeting', 'Hi!!!').render('index', { title:
+    'Express' });
+    });
 /* Жанр рок*/
 router.get('/rok', function(req, res, next) {
   res.render('music', {
