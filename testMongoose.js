985b19bfc7c684fa05c6cb9d985d590174e7f9a7
EdemBatalow <batalow.e.i23@gmail.com>
2024-11-13 12:48:26 +0300

    пункт 7_3

--- testMongoose.js
+++ testMongoose.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-var schema = mongoose.Schema({ name: String })
-schema.methods.rok = function(){
-    console.log(this.name + " ЕЕЕЕЕЕ")
-    }
-const music = mongoose.model('music', schema);
-const song = new music({ name: 'Рок:' });
-song.save().then(() => song.rok());
+ mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
+ var Music = require('./models/music.js').Music
+ var music  = new Music({
+ title: "Рок",
+ nick: "rok"
+ })
+ music.save();
