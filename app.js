3bb8067594ca88a8ad713c2bf980b0b3f853e91f
Edem <batalow.e.i23@gmail.com>
2024-11-17 16:43:11 +0000

    Пункт 9_5

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ req.session.counter = req.session.counter + 1 || 1
 next()
     })
       
-
+app.use(require("./middlewares/createMenu.js"))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/music', music);
