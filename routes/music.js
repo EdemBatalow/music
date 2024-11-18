82b2f465e3b32d3ce9c739d8d21588caf456576a
Edem <batalow.e.i23@gmail.com>
2024-11-18 06:59:49 +0000

    Пункт 10_8

--- routes/music.js
+++ routes/music.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var Music = require('../models/music').Music;
+var checkAuth = require("../middlewares/checkAuth.js");
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('Новый маршрутизатор, для маршрутов, начинающихся с musics');
     });
 
-router.get("/:nick", async function(req, res, next) {
+/* Страница котов */
+router.get("/:nick", checkAuth, async function(req, res, next) {
     var musics = await Music.find({nick: req.params.nick});
     console.log(musics)
     if(!musics.length) return next(new Error("Нет такого жанра"))
